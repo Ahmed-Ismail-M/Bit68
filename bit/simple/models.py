6c73e4002a65fb60694b4235abf1a602cdf074ce
ahmed <ahmedismailm85@gmail.com>
2022-01-14 15:31:07 +0200

    main

--- bit/simple/models.py
+++ bit/simple/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
 # Create your models here.
 
 class User(AbstractUser):
     pass
+
 
 class Product(models.Model):
     """ create a product model """
